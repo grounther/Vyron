1637c44a53552a57c082e571d764c6353de8e906
Oscar <o.kelder.raalte@gmail.com>
2026-05-18 15:38:41 +0200

    Fix Google products XML route handler

--- app/api/google/products.xml/route.ts
+++ app/api/google/products.xml/route.ts
@@ -1,1 +1,8 @@
-export { GET, dynamic, runtime } from '../merchant-feed/route'
+import { GET as merchantFeedGET } from '../merchant-feed/route'
+
+export const runtime = 'nodejs'
+export const dynamic = 'force-dynamic'
+
+export async function GET() {
+  return merchantFeedGET()
+}
